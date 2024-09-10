bd04797e8516e350b74945b152a7d5dfda4ae22e
doheeMac <thsehgml37@korea.ac.kr>
2024-09-11 04:18:39 +0900

    feat: add and applied global state managements

--- src/lib/globalState/index.ts
+++ src/lib/globalState/index.ts
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useDebugValue, useEffect, useState } from "react";
 
-type InitialGlobalState = Record<(string | number) & symbol, unknown>;
+export type InitialGlobalState = Record<(string | number) & symbol, unknown>;
 
 export type GlobalStateStore<T extends InitialGlobalState> = {
+  getInitialState: () => T;
   getState: () => T;
-  setState: (action: T | ((prev: T) => T)) => void;
+  setState: (action: (prev: T) => T) => void;
   subscribe: (callback: () => void) => () => void;
 };
 
@@ -14,9 +15,10 @@ export const createGlobalStateStore = <T extends InitialGlobalState>(
   let state = initialState;
   const callbacks = new Set<() => void>();
 
+  const getInitialState = () => initialState;
   const getState = () => state;
 
-  const setState = (nextState: T | ((prev: T) => T)) => {
+  const setState = (nextState: (prev: T) => T) => {
     if (typeof nextState === "function") {
       state = (nextState as (prev: T) => T)(state);
     } else {
@@ -31,7 +33,7 @@ export const createGlobalStateStore = <T extends InitialGlobalState>(
     return () => callbacks.delete(callback);
   };
 
-  return { getState, setState, subscribe };
+  return { getInitialState, getState, setState, subscribe };
 };
 
 export const useGlobalStateStore = <T extends InitialGlobalState>({
@@ -39,18 +41,18 @@ export const useGlobalStateStore = <T extends InitialGlobalState>({
   selector,
 }: {
   store: GlobalStateStore<T>;
-  selector: (state: T) => Partial<T>;
-}) => {
+  selector: (state: T) => T[keyof T];
+}): [T[keyof T], GlobalStateStore<T>["setState"]] => {
   const [state, setState] = useState(() => selector(store.getState()));
 
   useEffect(() => {
     const unsubscribe = store.subscribe(() =>
       setState(selector(store.getState()))
     );
-
     setState(selector(store.getState()));
     return () => unsubscribe();
-  }, [store, selector]);
+  }, []);
 
+  useDebugValue(state);
   return [state, store.setState];
 };
